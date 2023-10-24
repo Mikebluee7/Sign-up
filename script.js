198de1a9a982ff8e28375ae6f8767f2f62bc6362
MagicMajk13 <michalpietrzak385@gmail.com>
2023-10-24 21:18:17 +0200

    AddFunctions

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const previousOperand = document.querySelector('.previous-operand'); 
 const currentOperand = document.querySelector('.current-operand'); 
-const spanTwo = document.querySelector('.span-two');
+const clear = document.querySelector('.span-two');
 const operators = document.querySelectorAll('.operator'); 
-
+const operatorButton = document.querySelectorAll('.button');
+const equalButton = document.querySelector('.equal');
+const mathSign = document.querySelector('.mathSign');
 let result = '';
 
+function displayNumbers() {
+    if(this.textContent === '.' && currentOperand.innerHTML.includes('.')) return;
+    if(this.textContent === '.' && currentOperand.innerHTML === '') return currentOperand.innerHTML = '.0'
+
+    currentOperand.innerHTML += this.textContent;
+}
+
+function operate {
+    if(currentOperand.innerHTML === '' && this.textContent === '-'){
+        currentOperand.innerHTML = '-';
+        return;
+    }
+}
+
+
+function clearScreen {
+
+}
+
+
+function showResult {
+
+}
 
 
 
@@ -16,15 +41,12 @@ let result = '';
 
 
 
-
-
-
-
-
-
-
+operatorButton.forEach((button) =>{
+    button.addEventListener('click', displayNumbers)
+})
 
 operators.forEach(operator => operator.addEventListener('click', operate));
 
-spanTwo.addEventListener('click', clearScreen);
+clear.addEventListener('click', clearScreen);
 
+equalButton.addEventListener('click', showResult);
